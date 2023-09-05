b55c7fae382fc39a2a116ff7dc448411ee093ab6
paseri3739 <tael43@icloud.com>
2023-09-05 17:00:24 +0900

    downloader

--- scrape_e621.py
+++ scrape_e621.py
@@ -1,5 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
+import os
+import time
 
 
 def scrape_url(url):
@@ -24,6 +26,28 @@ def scrape_url(url):
     return large_file_urls
 
 
+def download_images(large_file_urls):
+    # ./imgディレクトリが存在しない場合は作成
+    if not os.path.exists("./img"):
+        os.mkdir("./img")
+
+    for i, url in enumerate(large_file_urls):
+        # 画像をダウンロード
+        response = requests.get(url)
+
+        # ステータスコードが200 (OK) でなければ例外を発生させる
+        response.raise_for_status()
+
+        # 画像を保存
+        with open(f"./img/image_{i}.jpg", "wb") as f:
+            f.write(response.content)
+
+        print(f"Downloaded {url} as image_{i}.jpg")
+
+        # 2秒待機
+        time.sleep(2)
+
+
 # スクレイピング対象のURL
 url = "https://e621.net/posts?tags=kuromu"
 
@@ -31,5 +55,9 @@ url = "https://e621.net/posts?tags=kuromu"
 try:
     large_file_urls = scrape_url(url)
     print("Found data-large-file-url(s):", large_file_urls)
+    print(f"There are {len(large_file_urls)} URL(s) in the page.")
+
+    # 画像をダウンロード
+    download_images(large_file_urls)
 except Exception as e:
     print("An error occurred:", e)
